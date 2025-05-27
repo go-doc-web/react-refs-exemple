45d62a739bcfa80556fa0a37a2f4b69a5c25e69c
Oleh Hubskiy <gubskyolegod@gmail.com>
2025-05-27 17:08:32 +0300

    Adding Challenges to the  Project

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,19 +2,19 @@ import { useState, useRef } from "react";
 
 export default function Player() {
   const inputName = useRef(null);
-  const count = useRef(0);
-  const [enterdInputName, setEnterdInputName] = useState("");
+
+  const [enterdInputName, setEnterdInputName] = useState(null);
 
   const handleClick = () => {
     setEnterdInputName(inputName.current.value);
-    count.current = count.current + 1;
+    inputName.current.value = "";
   };
   return (
     <section id="player">
       <h2>Welcome {enterdInputName ?? "unknown entity"} </h2>
       <p>
         <input type="text" ref={inputName} />
-        <button onClick={handleClick}>Set Name - {count.current}</button>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
